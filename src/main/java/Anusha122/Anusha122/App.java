92493fd1676943f77ec9e1967a2efe8d3a552011
anusha3202 <anushavigrahala3202@gmail.com>
2022-12-23 15:21:26 +0530

    First Commit

---
+++ src/main/java/Anusha122/Anusha122/App.java
@@ -0,0 +1,13 @@
+package Anusha122.Anusha122;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Anusha122/Anusha122/AppTest.java
@@ -0,0 +1,20 @@
+package Anusha122.Anusha122;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
